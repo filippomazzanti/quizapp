f7fa86bef29785e03bb0cd5f79090d68467d12e9
filippomazzanti <filippomazzantibeats@gmail.com>
2026-08-04 00:03:35 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
 // Dopo aver caricato su GitHub una nuova versione di index.html, cambia il
 // numero di CACHE qui sotto (v1 -> v2): forza la pulizia della cache vecchia.
 
-const CACHE = "quiz-sdm-v2";
+const CACHE = "quiz-sdm-v3";
 const ASSETS = [
   "./",
   "./index.html",
